855d039d270aad085d7f2b41fcec23ec59c9ee0c
Niraj Raj Pandey <niraajrp@gmail.com>
2017-12-29 10:09:30 +0545

    initial commit

---
+++ dump_tweets.py
@@ -0,0 +1,42 @@
+import tweepy
+from pprint import pprint
+import curd_operations
+
+ACCESS_TOKEN = '940132138208280576-gbFItpMERsbIOv0iJOlELBClNqBB0rI'
+ACCESS_SECRET = 'fHjVVOcx5HcJDdG3yna41yZdo5AGWDBkYegO3aePGnMVm'
+CONSUMER_KEY = '5szr0YwpLwFLSYbFDEhv82ZoG'
+CONSUMER_SECRET = 'ompvELFIGIdQ9K31jpGdI1ehPX2JOLoYiZ0zTGh8KmUHFswBde'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search(q='#NetNeutrality')
+for tweet in public_tweets:
+    user_id = tweet.id
+    tweet_text = tweet.text
+    tweet_date = tweet.created_at
+    pprint(tweet)
+    try:
+        curd_operations.insert_tweets([user_id, tweet_text, tweet_date])
+    except:
+        print("Error Uploading to database")
+
+
+    pass
+print("********************************************************************************")
+
+# import json
+# from database_connection import curd_operations
+# import requests
+# from pprint import pprint
+# def main():
+#     response = requests.get(
+#         "https://www.googleapis.com/books/v1/volumes?q=history")
+#     print(response)
+#     result = response.json()
+#     print(result['items'][5['volumeInfo'])
+#     print("----------------------------------")
+# curd_operations.insert_tweets()
+# if __name__ == '__main__':
+#     main()
